41d599a0efee2e6a09041e3a748082753f18a20a
Maciej Rybak <maciek.rybak95@gmail.com>
2024-05-07 20:17:57 +0200

    create Image model

--- database/migrations/2024_05_07_181710_create_images_table.php
+++ database/migrations/2024_05_07_181710_create_images_table.php
@@ -11,12 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->text('description');
-            $table->unsignedInteger('price');
-            $table->unsignedInteger('main_image_id')->nullable();
             $table->timestamps();
         });
     }
@@ -26,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('images');
     }
 };
