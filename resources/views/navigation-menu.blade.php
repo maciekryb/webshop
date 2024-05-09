09c6d5b51aa440190bdcf3db001e571410a31071
Maciej Rybak <maciek.rybak95@gmail.com>
2024-05-09 10:16:59 +0200

    update navigation names

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -13,7 +13,7 @@
         <!-- Navigation Links -->
         <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
           <x-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
-            {{ __('home') }}
+            {{ __('Home') }}
           </x-nav-link>
         </div>
       </div>
@@ -151,7 +151,7 @@
   <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
     <div class="pt-2 pb-3 space-y-1">
       <x-responsive-nav-link href="{{ route('home') }}" :active="request()->routeIs('home')">
-        {{ __('home') }}
+        {{ __('Home') }}
       </x-responsive-nav-link>
     </div>
 
