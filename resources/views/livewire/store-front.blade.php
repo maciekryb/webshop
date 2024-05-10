fc22eaaa09828a886b9b113b16ca38d4a8b7b908
Maciej Rybak <maciek.rybak95@gmail.com>
2024-05-10 20:03:04 +0200

    update stor-front  csss blade file

--- resources/views/livewire/store-front.blade.php
+++ resources/views/livewire/store-front.blade.php
@@ -1,7 +1,9 @@
 <div class="grid grid-cols-4 gap-4 mt-12">
   @foreach ($this->products as $product)
     <div>
-      {{ $product->name }}
+      <img src="{{ $product->image->path }}" alt="tu jest image">
+      <h2 class ="font-medium text-lg"> {{ $product->name }} </h2>
+      <span class ="text-gray-700 text-sm"> {{ $product->price }} </span>
     </div>
   @endforeach
 </div>
