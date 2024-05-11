1eb874338f89abd67417e58edba268f6f326b11f
Maciej Rybak <maciek.rybak95@gmail.com>
2024-05-11 15:08:44 +0200

    update add method in Action AddProductVariantToCart

--- app/Actions/Webshop/AddProductVariantToCart.php
+++ app/Actions/Webshop/AddProductVariantToCart.php
@@ -6,12 +6,12 @@ use App\Factories\CartFactory;
 
 class AddProductVariantToCart
 {
-
-    public function add($variantId)
+    public function add($variantId, $quantity = 1, $cart = null)
     {
-        CartFactory::make()->items()->firstOrCreate(
-            ['product_variant_id' => $variantId,],
-            ['quantity' => 0]
-        )->increment('quantity');
+        // If cart is defined we use it or fallback to use the factory.
+        ($cart ?: CartFactory::make())->items()->firstOrCreate(
+            ["product_variant_id" => $variantId],
+            ["quantity" => 0]
+        )->increment('quantity', $quantity);
     }
 }
