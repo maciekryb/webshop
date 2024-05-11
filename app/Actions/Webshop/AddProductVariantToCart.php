175988ecc1052b439ef8f77a63728c0eda237449
Maciej Rybak <maciek.rybak95@gmail.com>
2024-05-11 13:08:46 +0200

    update add method in AddProductVariantToCart

--- app/Actions/Webshop/AddProductVariantToCart.php
+++ app/Actions/Webshop/AddProductVariantToCart.php
@@ -9,9 +9,9 @@ class AddProductVariantToCart
 
     public function add($variantId)
     {
-        CartFactory::make()->items()->create([
-            'product_variant_id' => $variantId,
-            'quantity' => 1,
-        ]);
+        CartFactory::make()->items()->firstOrCreate(
+            ['product_variant_id' => $variantId,],
+            ['quantity' => 0]
+        )->increment('quantity');
     }
 }
