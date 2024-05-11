f1b79d4a7cf3d972420bca69d5454372402097e1
Maciej Rybak <maciek.rybak95@gmail.com>
2024-05-11 12:14:07 +0200

    create livewire cart component

---
+++ app/Livewire/Cart.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Factories\CartFactory;
+use Livewire\Component;
+
+class Cart extends Component
+{
+
+    public function getItemsProperty()
+    {
+        return CartFactory::make()->items;
+    }
+
+    public function render()
+    {
+        return view('livewire.cart');
+    }
+}
