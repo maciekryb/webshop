a07497e1f84cda2390f36858ac80a2da598a10be
Maciej Rybak <maciek.rybak95@gmail.com>
2024-05-18 13:50:22 +0200

    create action HandleCheckoutSessionCompleted

---
+++ app/Actions/Webshop/HandleCheckoutSessionCompleted.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions\Webshop;
+
+use App\Models\User;
+use Laravel\Cashier\Cashier;
+
+class HandleCheckoutSessionCompleted
+{
+    public function handle($sessionId)
+    {
+        $session = Cashier::stripe()->checkout->sessions->retrieve($sessionId);
+        $user = User::find($session->metadata->user_id);
+    }
+}
