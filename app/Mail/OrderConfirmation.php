f9dc1fa4833ff3645917192b751a1b5de345cdc0
Maciej Rybak <maciek.rybak95@gmail.com>
2024-05-18 18:29:18 +0200

    switch Order mail type to markdown

--- app/Mail/OrderConfirmation.php
+++ app/Mail/OrderConfirmation.php
@@ -38,7 +38,7 @@ class OrderConfirmation extends Mailable
     public function content(): Content
     {
         return new Content(
-            view: 'emails.order-confirmation',
+            markdown: 'emails.order-confirmation',
         );
     }
 
