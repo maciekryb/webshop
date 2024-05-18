43c8efd532a9bb11dbb35bc3452b0771c2196197
Maciej Rybak <maciek.rybak95@gmail.com>
2024-05-18 16:24:26 +0200

    create CheckoutStatus livewire component

---
+++ app/Livewire/CheckoutStatus.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class CheckoutStatus extends Component
+{
+    public function render()
+    {
+        return view('livewire.checkout-status');
+    }
+}
