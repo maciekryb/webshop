677d93829c9fec32ce3dcc7cd138990514ee272b
Maciej Rybak <maciek.rybak95@gmail.com>
2024-05-19 16:13:42 +0200

    add touch method to action AddProductVariantToCart

--- app/Actions/Webshop/AddProductVariantToCart.php
+++ app/Actions/Webshop/AddProductVariantToCart.php
@@ -9,9 +9,11 @@ class AddProductVariantToCart
     public function add($variantId, $quantity = 1, $cart = null)
     {
         // If cart is defined we use it or fallback to use the factory.
-        ($cart ?: CartFactory::make())->items()->firstOrCreate(
+        $item = ($cart ?: CartFactory::make())->items()->firstOrCreate(
             ["product_variant_id" => $variantId],
             ["quantity" => 0]
-        )->increment('quantity', $quantity);
+        );
+        $item ->increment('quantity', $quantity);
+        $item ->touch();
     }
 }
